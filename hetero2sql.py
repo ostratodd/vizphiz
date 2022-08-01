570ad4cc34ad34ad94875eefe2a43d0dfeb7bbd2
Todd Oakley <oakley@lifesci.ucsb.edu>
2022-08-01 16:00:05 -0700

    changing hetero2 script

--- hetero2sql.py
+++ hetero2sql.py
@@ -16,14 +16,13 @@ for line in Lines:
     columns = line.split("\t")
     print(columns)
     mycursor = mydb.cursor()
-
     sql = "INSERT INTO heterologous (hetid, genus, species, accession, mutations, lamdamax, error, cellculture, purification, spectrum, sourcetype, refid) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     val = (columns[0], columns[1], columns[2], columns[3], columns[4], columns[5], columns[6], columns[7], columns[8], columns[9], columns[10], columns[11])
     print(sql)
     print(val)
 
-#    mycursor.execute(sql, val)
+    mycursor.execute(sql, val)
 
-#    mydb.commit()
+    mydb.commit()
 
     print(mycursor.rowcount, "record inserted.")
