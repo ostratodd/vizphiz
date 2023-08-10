814af8638629e5f587daafcf3ab76732cccbbb69
k-wavy <105947657+k-wavy@users.noreply.github.com>
2023-08-09 19:12:51 -0700

    mySQL integration into jupyter nb

---
+++ old_misc_sort_later/hetero2sql_pw_required.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password= "Geass5566!!",
+  database= "vizphiz"
+)
+
+#read file for data
+file1 = open('heterologous.tsv', 'r', encoding="utf8")
+Lines = file1.readlines()
+
+count=0
+for line in Lines:
+    columns = line.split("\t")
+    print(columns)
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO opsins.heterologous (hetid, genus, species, accession, mutations, lamdamax, error, cellculture, purification, spectrum, sourcetype, refid) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (columns[0], columns[1], columns[2], columns[3], columns[4], columns[5], columns[6], columns[7], columns[8], columns[9], columns[10], columns[11])
+    print(sql)
+    print(val)
+
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "record inserted.")
